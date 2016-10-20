9d28506b11d89bf9ead7a0fe2b3ea7f4be6f89ff
Russell Folk <russell@clevercoder.guru>
2016-10-19 20:38:45 -0600

    Documenting thread_info.h
    This includes documentation on how the thread_info structure is used by the writer application.

--- src/thread_info.h
+++ src/thread_info.h
@@ -1,5 +1,14 @@
-
-
+/**
+  * Thread Info saves information required by each thread in the writer program
+  *
+  * When the writer is running, each thread is responsible for creating a record
+  * containing its thread id, the number of records created thus far, and
+  * writing them to a file.
+  *
+  * @member thread_id  the local id for the thread working
+  * @member record_num the number of records written thus far
+  * @member fd         the file descriptor of the file to be written to
+  */
 typedef struct saved_thread_info
 {
 	long thread_id;
