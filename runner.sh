1c311597c698f227c9cbfd72e04d35cdcf39754b
Russell Folk <russell@clevercoder.guru>
2016-10-19 22:31:50 -0600

    Extending runner to work for entire experiment
    Runner will now work for the entire experiment set instead of just the writer portion. As per assignment requirements.

--- runner.sh
+++ runner.sh
@@ -1,13 +1,32 @@
 #!/usr/bin/env bash
 
-PROGRAM="run"
-WORKERS=2
-TIME=15
+WRITER="writer"
+CHECKER="checker"
+CREATOR="./create_device.sh"
+FILE="device-file"
+LOCATION="./bin/"
+WORKERS=16
+TIME=60
+DEBUG=false
 
-while getopts "p:t:w:" opt; do
+USAGE="To run: ./run_experiments -l <file locations> -f <device file>
+-r <writer> -c <checker> -t <seconds to write> -w <# threads to use>\n\
+If no arguments are supplied: file location = ./bin/, device file = device-file,
+ writer = writer, checker = checker, seconds = 60, threads = 16"
+
+while getopts "l:f:r:c:t:w:?:d" opt; do
         case $opt in
-                p)
-                        PROGRAM=${OPTARG}
+                l)
+                        LOCATION="./"${OPTARG}
+                        ;;
+                f)
+                        FILE=${OPTARG}
+                        ;;
+                r)
+                        WRITER=${OPTARG}
+                        ;;
+                c)
+                        CHECKER=${OPTARG}
                         ;;
                 t)
 			TIME=${OPTARG}
@@ -16,9 +35,44 @@ while getopts "p:t:w:" opt; do
                         WORKERS=${OPTARG}
                         ;;
                 \?)
-                        echo "Invalid option: -$OPTARG" >&2
+                        echo -e ${USAGE}
+                        exit
+                        ;;
+                d)
+                        DEBUG=true
                         ;;
         esac
 done
 
-./bin/${PROGRAM} -w ${WORKERS} > tmp & sleep ${TIME}; kill -9 $!
+# clean out old files
+make clean
+
+# compile the programs
+make
+
+echo -e "DEBUG IS ${DEBUG}"
+# create the device file
+if [ ${DEBUG} = false ]; then
+        echo "no debug"
+        ${CREATOR} -f ${FILE} -l ${LOCATION}
+else
+        ${CREATOR} -d -f ${FILE} -l ${LOCATION}
+fi
+
+# run the writer for t seconds
+if [ ${DEBUG} = false ]; then
+        echo "no debug"
+        ${LOCATION}${WRITER} -w ${WORKERS} -f ${LOCATION}${FILE} &
+        sleep ${TIME}
+        kill -9 $!
+else
+        ${LOCATION}${WRITER} -d -w ${WORKERS} -f ${LOCATION}${FILE} > "writer_debug.txt"
+fi
+
+# run the checker
+if [ ${DEBUG} = false ]; then
+        echo "no debug"
+        ${LOCATION}${CHECKER} -f ${LOCATION}${FILE}
+else
+        ${LOCATION}${CHECKER} -d -f ${LOCATION}${FILE} > "checker_debug.txt"
+fi
