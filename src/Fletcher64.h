6f8635aa30244e21c8ab76a1709390659983ffee
Russell Folk <russell@clevercoder.guru>
2016-10-20 11:22:09 -0600

    Document Fletcher64 checksum

--- src/Fletcher64.h
+++ src/Fletcher64.h
@@ -11,6 +11,9 @@
  * ceeding IND_TAIL_CHECKSUM, the last value of the record. The computed check-
  * sum is then returned to the requesting interface and used for operations.
  *
+ * Implementation derived from:
+ * https://en.wikipedia.org/wiki/Fletcher%27s_checksum
+ *
  * @param  record   This should be an array of 1024 elements
  * @return          The checksum for elements 1-1022 (not including head/tail
  *                  checksums).
