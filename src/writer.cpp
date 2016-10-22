3410b9254c116039aca2017e085afc366a2257bb
Russell Folk <russell@clevercoder.guru>
2016-10-21 18:21:28 -0600

    Document and clean up writer
    I have gone through and documented and cleaned up the writer program so that everything is clearer and more apparent.

--- src/writer.cpp
+++ src/writer.cpp
@@ -1,29 +1,38 @@
 #include "writer.h"
 
+/**
+ * device-file record writer
+ *
+ * @param  argc the number of flags and arguments provided
+ * @param  argv the flags and arguments provided
+ * @return      result code for the program running
+ */
 int main(int argc, char **argv)
 {
 	// required for argument parsing
-	int fflag = 0;
+	bool eflag = false; // exit flag, display help was requested
+	bool fflag = false;  // default filename flag
 	int c;
-	const char * def_filename = "device-file";
 	char * filename;
 	extern char *optarg;
 	extern int optind;
-	//static char usage[] = "usage: %s -f filename\n";
-	const int def_num_threads = 2;
-	int num_threads;
+	int num_threads = 0;
+	std::string usage = "To run: ./bin/writer -f <device file> -w <# threads to use> -d (to enable debug)\n"
+	                    "If no arguments are supplied: device file = bin/device-file, # threads = 4, debug = off";
 
 	// permanent values
 	int num_records;
 	int fd;
+	const char * def_filename = "bin/device-file";
+	const int def_num_threads = 4;
 
-	while ((c = getopt(argc, argv, "w:f:d")) != -1)
+	while ((c = getopt(argc, argv, "w:f:d?")) != -1)
 	{
 		switch (c)
 		{
 			case 'f':
 				filename = optarg;
-				fflag = 1;
+				fflag = true;
 				break;
 			case 'w':
 				num_threads = atoi(optarg);
@@ -31,62 +40,78 @@ int main(int argc, char **argv)
 			case 'd':
 				dflag = true;
 				break;
+			case '?':
+				std::cout << usage << std::endl;
+				eflag = true;
 			default:
 				// implement other options, such as help, etc.
 				break;
 		}
 	}
 
-	if (fflag == 0)
-	{
-		// need to implement default filename
-		filename = strdup(def_filename);
-	}
+	// Need to exit program if '?' argument is given
+	if (eflag)
+		exit(0);
 
-	if (!num_threads)
+	// Need to implement default filename
+	if (fflag)
+		filename = strdup(def_filename);
+
+	// Need to have a sane number of threads
+	if (num_threads < 1 || num_threads > 100)
 		num_threads = def_num_threads;
 
+	// open the file and set the descriptor
 	fd = open_file(filename);
 
+	// figure out how many records can exist in the device-file
 	file_size = (int) get_file_size(fd);
 	num_records = file_size / RECORD_SIZE;
 	first_record = 0;
 	last_record = num_records;
 
+	// debug output
 	if (dflag)
 	{
 		std::cout << "File range is 0 to " << file_size << "." << std::endl;
 		std::cout << "Number of possible records: " << num_records << std::endl;
 	}
 
-	int rc;
-	pthread_t threads[num_threads];
-	thread_info thread_data[num_threads];
+	int thread_error; // pthread_create error code
+	pthread_t threads[num_threads]; // array of threads that will be working
+	thread_info thread_data[num_threads]; // array of information needed by each thread
+
+	// create threads and start writing records
 	for (int t = 0; t < num_threads; t++)
 	{
+		// initialize thread-centric data
 		thread_data[t].thread_id = t;
 		thread_data[t].record_num = 1;
 		thread_data[t].fd = fd;
-		rc = pthread_create(&threads[t], NULL, worker_thread_init, (void *) &thread_data[t]);
-		if (rc)
+		// create threads, start writing records
+		thread_error = pthread_create(&threads[t], NULL, worker_thread_init, (void *) &thread_data[t]);
+		if (thread_error)
 		{
-			printf("ERROR; return code from pthread_create() is %d\n", rc);
+			printf("ERROR; return code from pthread_create() is %d\n", thread_error);
 			exit(-1);
 		}
 	}
 
+	// wait for all threads to complete their task
 	for (int t = 0; t < num_threads; t++)
 		pthread_join(threads[t], NULL);
 
-	pthread_exit(NULL);
+	// clean up and exit
 	close(fd);
 	return 0;
 }
 
 void * worker_thread_init(void * thread_arg)
 {
+	// parse the thread-centric data
 	thread_info * thread_data;
 	thread_data = (thread_info *) thread_arg;
+	// if in debug mode, print 10 records, else run indefinitely
 	if (dflag)
 		for (int i = 0; i < 10; i++)
 			create_record(thread_data);
@@ -101,62 +126,97 @@ void create_record(thread_info * record_data)
 	// record the time this record is started
 	auto since_epoch = std::chrono::high_resolution_clock::now().time_since_epoch();
 	auto nanos = std::chrono::duration_cast<std::chrono::nanoseconds>(since_epoch).count();
-	long record_size = RECORD_SIZE / sizeof(long);
-	long size = record_size * sizeof(long);
-	long * record = (long *) malloc(size);
-	long address = intRand();
+
+	// create an array of longs that comprise a complete record
+	long * record = (long *) malloc(RECORD_SIZE);
+
+	// collect the address for the record
+	long address = generate_address();
+
+	// store the checksum of the generated record for validation
 	long checksum;
 
+	// fill known / constant record data
 	record[IND_THREAD_ID] = record_data->thread_id;
 	record[IND_RECORD_NUM] = record_data->record_num;
 	record[IND_RECORD_ADDRESS] = address;
 	record[IND_TIMESTAMP] = (long) nanos;
-	for (int i = IND_FIRST_RANDOM_RECORD; i < record_size - 1; i++)
+
+	// generate "random data"
+	for (int i = IND_FIRST_RANDOM_RECORD; i < NUM_PIECES - 1; i++)
 	{
 		record[i] = record[i-1] + record[0] * record[1] * i + record[2];
 	}
 
+	// checksum the record and fill in the blanks where required
 	checksum = Fletcher64(record);
 	record[IND_TAIL_CHECKSUM] = checksum;
 	record[IND_HEAD_CHECKSUM] = checksum;
 
+	// debug output
 	if (dflag)
-		std::cout << "Record: " << record[IND_THREAD_ID] << " " << record[IND_RECORD_NUM] << " " << record[IND_RECORD_ADDRESS] << " " << record[IND_TIMESTAMP] << " " << checksum << std::endl;
-	size_t msg = write_record(record_data, record, size);
+		std::cout << "Record: " << record[IND_THREAD_ID] << " " << record[IND_RECORD_NUM] << " "
+		          << record[IND_RECORD_ADDRESS] << " " << record[IND_TIMESTAMP] << " " << checksum << std::endl;
+
+	// write the created record to the device-file
+	size_t msg = write_record(record_data, record, RECORD_SIZE);
+
+	// debug output
 	if (dflag)
 		std::cout << "msg " << msg << std::endl;
+
+	// increment the number of records created
 	record_data->record_num += 1;
+
+	// clean up memory
 	free(record);
 }
 
 size_t write_record(thread_info * record_data, long * record, long size)
 {
+	// parse the required arguments to write a record to the device-file
 	size_t nbyte = size / NUM_PIECES;
 	off_t offset = record[IND_RECORD_ADDRESS] * RECORD_SIZE;
 	int fd = record_data->fd;
 
+	// debug output
 	if (dflag)
 		std::cout << "fd " << fd << " nbyte " << nbyte << " offset " << offset << std::endl;
 
-	size_t msg = 0;
+	size_t msg = 0; // number of bytes successfully written
+
+	// write all the pieces of the record, sequentially, to the device-file
 	for (int piece = 0; piece < NUM_PIECES; piece++)
 	{
+		// debug output
 		if (dflag)
 			if (piece < IND_FIRST_RANDOM_RECORD && piece != IND_TAIL_CHECKSUM)
 				std::cout << record[piece] << " " << "offset: " << offset << " ";
+		// write single long and increment number of bytes written
 		msg += pwrite(fd, &record[piece], nbyte, offset);
+		// increase offset so that it's written to the next spot
 		offset += (off_t) nbyte;
 	}
+
+	// debug output
 	if (dflag)
 		std::cout << std::endl << "wrote: " << msg << std::endl;
 
-	return msg;
+	return msg; // should be 8192 (8KB)
 }
 
-int intRand(void)
+int generate_address(void)
 {
+	// declare static generator so it can be used by all threads
 	static thread_local std::mt19937* generator;
-	if (!generator) generator = new std::mt19937(clock());
+
+	// seed with current time
+	if (!generator)
+		generator = new std::mt19937(clock());
+
+	// utilize a uniform distribution
 	std::uniform_int_distribution<int> distribution(first_record, last_record);
+
+	// return the generated number
 	return distribution(*generator);
 }
--- src/writer.h
+++ src/writer.h
@@ -8,22 +8,90 @@
 #include <pthread.h>
 #include <chrono>
 
+// local libraries needed to run the writer
 #include "thread_info.h"
 #include "thread_local_random.h"
 #include "record_indexes.h"
 #include "Fletcher64.h"
 #include "file_lib.h"
 
+// number of pieces that are inside a record as long = 8, 8KB / 8 = 1024
 const int NUM_PIECES = 1024;
+// size of the record, specified by the assignment
 const int RECORD_SIZE = 8 * 1024;
 
+// global variables that contain the range for the random number generator for
+// an address to be made
 int first_record;
 int last_record;
+
+// the overall size of the device-file being operated upon
 int file_size;
 
+// flag that specifies whether or not to display debug information
 bool dflag = false;
 
-void create_record(thread_info *);
-size_t write_record(thread_info *, long *, long);
+/**
+  * Worker Thread Initialization
+  *
+  * worker_thread_init does one of two things, if debug mode is initialized, it
+  * will generate 10 records and write them to the supplied device-file. This
+  * allows for the device-file to be checked using the checker and verifying
+  * that the information written is generated correctly. If debug mode is not
+  * activated, then this will infinitely loop through creating and writing
+  * records to the supplied device-file until the program is killed.
+  *
+  * @param thread_arg is a thread_info that contains the recurring data about a
+  *                   record that must be included / incremented with every
+  *                   record created
+  */
 void * worker_thread_init(void *);
-int intRand(void);
+
+/**
+  * Create a record entry for the device file
+  *
+  * create_record generates a record that is setup using the record_indexes.h
+  * fields to place the required information. A record includes a checksum, the
+  * id of the thread that creates the record, the id of the record (incremented
+  * with every record starting with 1), the address the record is to be written
+  * to, the number of nanoseconds since the epoch when the record was created,
+  * and a set of computer generated data that is a set formula that can be later
+  * verified to see where a breakdown occurs.
+  *
+  * @param record_data contains the recurring data about a record that must be
+  *                    included / incremented with every record created
+  */
+void create_record(thread_info *);
+
+/**
+  * Write a Record to the supplied device-file
+  *
+  * Attempts to write a complete record to the supplied device-file. After much
+  * research it was found that pwrite() is an "atomic function" in that
+  * according to POSIX standards the command cannot be interrupted in the middle
+  * of a write. As such, a record is now split up into 1024 writes, 1 write per
+  * long contained in the record, and the writes are sequentially written to the
+  * device-file. At the end of the write, the total number of bytes written is
+  * returned. This SHOULD be 8192 bytes, but it is not guaranteed.
+  *
+  * @param record_data contains the recurring data about a record that must be
+  *                    included / incremented with every record created; needed
+  *                    for the file descriptor of the device-file
+  * @param record      a complete record to be written to the device-file
+  * @param size        the total size, in bytes, of the record to be written
+  * @return            the number of bytes that is written to the device-file
+  */
+size_t write_record(thread_info *, long *, long);
+
+/**
+  * Generate random record Address for device-file
+  *
+  * This is a thread safe implementation of the mt19937 standard library random
+  * number generator using a uniform distribution. The reason for a global rng
+  * is so that each thread will not recreate the same pattern of records to
+  * write to the device-file.
+  *
+  * @return  return an address location between the global first_record and
+  * last_record values of the device-file
+  */
+int generate_address(void);
